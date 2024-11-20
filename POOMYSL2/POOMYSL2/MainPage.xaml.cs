dd40ca5b406440a86e8901a66b2253b15a982f8f
emmenik200 <91954953+emmenik200@users.noreply.github.com>
2024-11-20 19:32:55 +0100

    Dodaj pliki projektów.

---
+++ POOMYSL2/POOMYSL2/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace POOMYSL2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int x = 0;
+        public void Usun(object sender, EventArgs e)
+        {
+            if (x > 0)  
+            {
+                x--;
+                ilosc.Text = x.ToString() + " Polubień";
+            }
+        }
+
+        public void Polub(object sender ,EventArgs e)
+        {
+            x++;
+            ilosc.Text = x.ToString()+" Polubień";
+        }
+    }
+}
